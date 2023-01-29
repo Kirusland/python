73198c4fe9b204833ef1b02ede942305ca66127c
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-01-29 18:57:22 +0300

    Update tkinter.py

--- p1/tkinter.py
+++ p1/tkinter.py
@@ -1,20 +1,22 @@
 from tkinter import *
+import time '''importing time for animation'''
 
 def greeting():
     print("Добро пожаловать")
 def good_night():
     print("Споки ноки, пупсик")
 
-window = Tk()
-canvas = Canvas(window, width = 500, height = 500)
+window = Tk()'''creating a window'''
+canvas = Canvas(window, width = 500, height = 500)'''creating canvas'''
 canvas.pack()
-canvas.create_line(0,0,500,500)
-blue_rect = canvas.create_rectangle(250,250,500,500, fill = "blue")
-red_rect = canvas.create_rectangle(20,20,250,250, fill = "red")
+canvas.create_line(0,0,500,500)'''creating line'''
+blue_rect = canvas.create_rectangle(250,250,500,500, fill = "blue")'''creating blue square'''
+red_rect = canvas.create_rectangle(20,20,250,250, fill = "red")'''creating red square'''
 a = 10
-while a<0:
-    canvas.move(red_rect,230,0)
-    a -= 1
+while a>
+    canvas.move(red_rect,23,0)
+    a = a-1
+    time.sleep(0.1)
 '''my_button = Button(window, text = "Привет", command = greeting)
 my_button.pack()
 sleep_button = Button(window, text = "Пора баиньки", command = good_night)
