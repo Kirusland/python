90ed6ef6429f23f52bad3e674e89b3cf07b40d9d
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-01-29 18:53:46 +0300

    Update tkinter.py

--- p1/tkinter.py
+++ p1/tkinter.py
@@ -11,7 +11,10 @@ canvas.pack()
 canvas.create_line(0,0,500,500)
 blue_rect = canvas.create_rectangle(250,250,500,500, fill = "blue")
 red_rect = canvas.create_rectangle(20,20,250,250, fill = "red")
-canvas.move(red_rect,230,0)
+a = 10
+while a<0:
+    canvas.move(red_rect,230,0)
+    a -= 1
 '''my_button = Button(window, text = "Привет", command = greeting)
 my_button.pack()
 sleep_button = Button(window, text = "Пора баиньки", command = good_night)
