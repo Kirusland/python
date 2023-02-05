367f7f82eb4248d11b80e59ea88da37bc872a341
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-02-05 18:58:44 +0300

    Update OOP.py

--- p1/OOP.py
+++ p1/OOP.py
@@ -6,3 +6,21 @@ class Car():
 
     def description(self):
         print("Марка авто:",self.make,"\nЦвет:", self.color, "\nМакс. скорость:", self.speed)
+
+    def ride(self):
+        print(self.color,self.make,"проехала на красный свет")
+class NesterovCar(Car):
+    def __init__(self,make,color,speed,rider):
+        super().__init__(make,color,speed)
+        self.rider = rider
+    def rider_change(self):
+        print("Дмитрий Егоров уволен")
+        self.rider = "Виталий Несоров собственной персоной"
+        print(self.make, self.color, "за рулём -", self.rider)
+        
+car1 = Car("BMW", "Black", 250)
+car1.description()
+car1.ride()
+ncar = NesterovCar("McPython", "Blue", 228, "Дмитрий Егоров")
+ncar.description()
+ncar.rider_change()
