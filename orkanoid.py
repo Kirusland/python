383cc8863a359e04c6045d62a2369a31026ec504
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-02-26 18:36:11 +0300

    Update orkanoid.py

--- orkanoid.py
+++ orkanoid.py
@@ -9,7 +9,9 @@ class Ball():
         self.directions = [-3,-2,-1,1,2,3]
         self.x = random.choice(self.directions)
         self.y = -1
-
+    def touch_platform(self,ball_pos):
+        platoform_pos = self.canvas.coords(self.platform.rect)
+        if ball_pos[2] >= platform)pos[0] and ball_pos[0] <= platform_pos[2]:
     def draw(self):
         self.canvas.move(self.oval, self.x, self.y)
         pos = self.canvas.coords(self.oval)
@@ -23,8 +25,9 @@ class Ball():
         if pos[0] <=0:
             self.x = 1
 class Platform():
-    def __init__(self,canvas,color):
+    def __init__(self,canvas,platform,color):
         self.canvas = canvas
+        self.platform = platform
         self.rect = canvas.create_rectangle(0,0,100,10,fill=color)
         self.canvas.move(self.rect,200,300)
         self.x = 0
@@ -36,6 +39,11 @@ class Platform():
         self.x = 2
     def draw(self):
         self.canvas.move(self.rect,self.x, 0)
+        pos = self.canvas.coords(self.rect)
+        if pos[0] <= 0:
+            self.x = 0
+        if pos[2] >= 500:
+            self.x = 0
             
  
 window = Tk()
@@ -45,7 +53,7 @@ window.wm_attributes("-topmost",1)
 canvas = Canvas(window, width = 500, height = 400)
 canvas.pack()
 platform = Platform(canvas,'red')
-ball = Ball(canvas,"blue")
+ball = Ball(canvas,platform,"blue")
 while True:
     ball.draw()
     platform.draw()
