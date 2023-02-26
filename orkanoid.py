80623a2e6642bbca20c6585a90d61e33b2799928
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-02-26 19:13:28 +0300

    Update orkanoid.py

--- orkanoid.py
+++ orkanoid.py
@@ -2,41 +2,48 @@ from tkinter import *
 import time
 import random
 class Ball():
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, platform,color):
         self.canvas = canvas
+        self.platform = platform
         self.oval = canvas.create_oval(10,10,25,25,fill = color)
         self.canvas.move(self.oval,240,100)
         self.directions = [-3,-2,-1,1,2,3]
         self.x = random.choice(self.directions)
-        self.y = -1
+        self.y = -3
+        self.touch_bottom = False
     def touch_platform(self,ball_pos):
-        platoform_pos = self.canvas.coords(self.platform.rect)
-        if ball_pos[2] >= platform)pos[0] and ball_pos[0] <= platform_pos[2]:
+        platform_pos = self.canvas.coords(self.platform.rect)
+        if ball_pos[2] >= platform_pos[0] and ball_pos[0] <= platform_pos[2]:
+            if ball_pos[3] >= platform_pos[1] and ball_pos[3] <= platform_pos[3]:
+                return True
+        return False
     def draw(self):
         self.canvas.move(self.oval, self.x, self.y)
         pos = self.canvas.coords(self.oval)
         print(pos)
         if pos[1] <=0:
-            self.y = 1
+            self.y = 4
         if pos[3] >= 400:
-            self.y = -1
+            self.touch_bottom = True
         if pos[2] >=500:
-            self.x = -1
+            self.x = -4
         if pos[0] <=0:
-            self.x = 1
+            self.x = 4
+        if self.touch_platform(pos) == True:
+            self.y = -4
 class Platform():
-    def __init__(self,canvas,platform,color):
+    def __init__(self,canvas,color):
         self.canvas = canvas
-        self.platform = platform
+        
         self.rect = canvas.create_rectangle(0,0,100,10,fill=color)
         self.canvas.move(self.rect,200,300)
         self.x = 0
         self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
     def turn_left(self,event):
-        self.x = -2
+        self.x = -5
     def turn_right(self,event):
-        self.x = 2
+        self.x = 5
     def draw(self):
         self.canvas.move(self.rect,self.x, 0)
         pos = self.canvas.coords(self.rect)
@@ -55,7 +62,13 @@ canvas.pack()
 platform = Platform(canvas,'red')
 ball = Ball(canvas,platform,"blue")
 while True:
-    ball.draw()
-    platform.draw()
+    if ball.touch_bottom == False:
+        ball.draw()
+        platform.draw()
+    else:
+        txt = Text(canvas, height = 10, width = 50, wrap = "word")
+        txt.insert(1.0, "Game over!")
+        txt.pack()
+        break
     window.update()
-    time.sleep(0.01)
+    time.sleep(0.001)
