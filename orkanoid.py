e3bd8fe7444bfdb0aa653a70b2889075ed36ecb8
Kirill <73121729+kirillmatytsyn@users.noreply.github.com>
2023-02-26 18:09:08 +0300

    Update orkanoid.py

--- orkanoid.py
+++ orkanoid.py
@@ -1,51 +1,53 @@
-
 from tkinter import *
 import time
 import random
-
 class Ball():
-    '''Класс мячика'''
     def __init__(self, canvas, color):
         self.canvas = canvas
-        self.oval = canvas.create_oval(10, 10, 25, 25, fill=color)
-        self.canvas.move(self.oval, 240, 100)
-        self.directions = [-3, -2, -1, 1, 2, 3]
+        self.oval = canvas.create_oval(10,10,25,25,fill = color)
+        self.canvas.move(self.oval,240,100)
+        self.directions = [-3,-2,-1,1,2,3]
         self.x = random.choice(self.directions)
         self.y = -1
 
     def draw(self):
-        self.canvas.move(self.oval, self.x, self.y)       
+        self.canvas.move(self.oval, self.x, self.y)
         pos = self.canvas.coords(self.oval)
         print(pos)
-        if pos[1] <= 0:
+        if pos[1] <=0:
             self.y = 1
         if pos[3] >= 400:
             self.y = -1
-        if pos[0] <= 0:
-            self.x = 1
-        if pos[2] >= 500:
+        if pos[2] >=500:
             self.x = -1
-
+        if pos[0] <=0:
+            self.x = 1
 class Platform():
-    '''Класс платформы'''
-    def __init__(self, canvas, color):
+    def __init__(self,canvas,color):
         self.canvas = canvas
-        self.rect = canvas.create_rectangle(0, 0, 100, 10, fill=color)
-        self.canvas.move(self.rect, 200, 300)
-        
-
-#--------------------------------MAIN-----------------------------------
+        self.rect = canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.rect,200,300)
+        self.x = 0
+        self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
+    def turn_left(self,event):
+        self.x = -2
+    def turn_right(self,event):
+        self.x = 2
+    def draw(self):
+        self.canvas.move(self.rect,self.x, 0)
+            
+ 
 window = Tk()
-window.title("Арканоид")
-window.resizable(0, 0)
-window.wm_attributes("-topmost", 1)
-canvas = Canvas(window, width=500, height=400)
+window.title("Nesterov game")
+window.resizable(0,0)
+window.wm_attributes("-topmost",1)
+canvas = Canvas(window, width = 500, height = 400)
 canvas.pack()
-
-ball = Ball(canvas, 'blue')
-platform = Platform(canvas, 'yellow')
-
+platform = Platform(canvas,'red')
+ball = Ball(canvas,"blue")
 while True:
     ball.draw()
+    platform.draw()
     window.update()
     time.sleep(0.01)
